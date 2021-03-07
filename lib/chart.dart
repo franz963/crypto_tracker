5c3ab3653828ea52e2eae543fad9a0e77538ce31
Eric Porter <14321247+franz963@users.noreply.github.com>
2021-03-07 15:37:10 -0500

    Updating chart with live data

--- lib/chart.dart
+++ lib/chart.dart
@@ -15,40 +15,49 @@ class _ChartState extends State<Chart> {
     return Expanded(
       child: Container(
         height: 200,
-        child: LineChart(
-          sampleData1(),
-          // swapAnimationDuration: const Duration(milliseconds: 250),
+        child: FutureBuilder<List<dynamic>>(
+          future: coinData(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return LineChart(
+                sampleData1(snapshot.data),
+                // swapAnimationDuration: const Duration(milliseconds: 250),
+              );
+            }
+            return CircularProgressIndicator();
+          },
         ),
       ),
     );
   }
 
-  LineChartData sampleData1() {
+  LineChartData sampleData1(List<dynamic> data) {
     return LineChartData(
       gridData: FlGridData(
         show: false,
       ),
       titlesData: FlTitlesData(
         bottomTitles: SideTitles(
-          showTitles: true,
-          reservedSize: 22,
-          getTextStyles: (value) => const TextStyle(
-            color: Color(0xff72719b),
-            fontWeight: FontWeight.bold,
-            fontSize: 16,
-          ),
-          margin: 10,
-          getTitles: (value) {
-            switch (value.toInt()) {
-              case 2:
-                return 'SEPT';
-              case 7:
-                return 'OCT';
-              case 12:
-                return 'DEC';
-            }
-            return '';
-          },
+          showTitles: false,
+          // reservedSize: 22,
+          // getTextStyles: (value) => const TextStyle(
+          //   color: Color(0xff72719b),
+          //   fontWeight: FontWeight.bold,
+          //   fontSize: 16,
+          // ),
+          // margin: 10,
+          // getTitles: (value) {
+          //   print(value);
+          //   switch (value.toInt()) {
+          //     case 2:
+          //       return 'SEPT';
+          //     case 7:
+          //       return 'OCT';
+          //     case 12:
+          //       return 'DEC';
+          //   }
+          //   return '';
+          // },
         ),
         leftTitles: SideTitles(
           showTitles: false,
@@ -57,26 +66,18 @@ class _ChartState extends State<Chart> {
       borderData: FlBorderData(
         show: false,
       ),
-      minX: 0,
-      maxX: 14,
-      maxY: 4,
-      minY: 0,
-      lineBarsData: linesBarData1(),
+      minX: findMin(data, 0) - 20000000,
+      maxX: findMax(data, 0) + 20000000,
+      maxY: findMax(data, 1) + 500,
+      minY: findMin(data, 1),
+      lineBarsData: linesBarData1(data),
     );
   }
 
-  List<LineChartBarData> linesBarData1() {
-    coinData();
+  List<LineChartBarData> linesBarData1(List<dynamic> data) {
+    // coinData();
     final LineChartBarData lineChartBarData1 = LineChartBarData(
-      spots: [
-        FlSpot(1, 1),
-        FlSpot(3, 1.5),
-        FlSpot(5, 1.4),
-        FlSpot(7, 3.4),
-        FlSpot(10, 2),
-        FlSpot(12, 2.2),
-        FlSpot(13, 1.8),
-      ],
+      spots: getSpotList(data),
       isCurved: true,
       colors: [
         const Color(0xff4af699),
@@ -93,29 +94,51 @@ class _ChartState extends State<Chart> {
     return [lineChartBarData1];
   }
 
-  Future<List<FlSpot>> coinData() async {
-    List<FlSpot> returnValue = [];
-
+  Future<List<dynamic>> coinData() async {
     final endpoint = Uri.https(
         'api.coingecko.com',
         'api/v3/coins/bitcoin/market_chart',
-        {'vs_currency': 'usd', 'days': '2', 'interval': 'daily'});
+        {'vs_currency': 'usd', 'days': '2'});
     print(endpoint);
     final response = await http.get(endpoint);
 
     if (response.statusCode == 200) {
       var jsonResponse = convert.jsonDecode(response.body);
-      var prices = jsonResponse['prices'];
-      for (final price in prices) {
-        var x = price[0];
-        var y = price[1];
-        final graphValue = FlSpot(x.toDouble(), y.toDouble());
-        returnValue.add(graphValue);
-      }
-      print(returnValue);
-      return returnValue;
+      return jsonResponse['prices'];
     } else {
       throw Exception('Failed to get coin data');
     }
+  }
+
+  double findMin(List<dynamic> data, int position) {
+    double min = -1;
+    for (final value in data) {
+      if (min == -1 || value[position] < min) {
+        min = value[position].toDouble();
+      }
+    }
+    return min;
+  }
+
+  double findMax(List<dynamic> data, int position) {
+    double max = -1;
+    for (final value in data) {
+      if (value[position] > max) {
+        max = value[position].toDouble();
+      }
+    }
+    return max;
+  }
+
+  List<FlSpot> getSpotList(List<dynamic> prices) {
+    List<FlSpot> returnValue = [];
+    for (final price in prices) {
+      var x = price[0];
+      var y = price[1];
+      final graphValue = FlSpot(x.toDouble(), y.toDouble());
+      returnValue.add(graphValue);
+    }
+    print(returnValue);
+    return returnValue;
   }
 }
