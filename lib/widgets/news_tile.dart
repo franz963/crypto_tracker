8804e409f5856f27e21077a28332a3f8ae0abdde
Eric Porter <14321247+franz963@users.noreply.github.com>
2021-03-14 12:37:37 -0400

    Adding ability to view news in browser

--- lib/widgets/news_tile.dart
+++ lib/widgets/news_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class NewsTile extends StatelessWidget {
   final String newsTile;
@@ -10,32 +11,40 @@ class NewsTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(left: 10, right: 10, bottom: 15, top: 15),
-      height: 100,
-      // width: 200,
-      decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            new BoxShadow(
-              color: Colors.black54,
-              blurRadius: 10,
+    return GestureDetector(
+      onTap: () {
+        _launchURL();
+      },
+      child: Container(
+        margin: EdgeInsets.only(left: 10, right: 10, bottom: 15, top: 15),
+        height: 100,
+        // width: 200,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              new BoxShadow(
+                color: Colors.black54,
+                blurRadius: 10,
+              ),
+            ],
+            borderRadius: BorderRadius.all(Radius.circular(15))),
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(5),
+                child: Text(
+                  (newsTile.length <= 100)
+                      ? newsTile
+                      : newsTile.substring(0, 100) + '...',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
             ),
+            Text(formatDate()),
           ],
-          borderRadius: BorderRadius.all(Radius.circular(15))),
-      child: Column(
-        children: [
-          Expanded(
-            child: Text(
-              (newsTile.length <= 100)
-                  ? newsTile
-                  : newsTile.substring(0, 100) + '...',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          Text(formatDate()),
-        ],
+        ),
       ),
     );
   }
@@ -43,5 +52,18 @@ class NewsTile extends StatelessWidget {
   String formatDate() {
     final DateFormat formatter = DateFormat('MM/dd/yyyy - H:mm:ss');
     return formatter.format(publishingDate);
+  }
+
+  Future<void> _launchURL() async {
+    if (await canLaunch(newsUrl)) {
+      await launch(
+        newsUrl,
+        forceSafariVC: true,
+        forceWebView: true,
+        headers: <String, String>{'my_header_key': 'my_header_value'},
+      );
+    } else {
+      throw 'Could not launch $newsUrl';
+    }
   }
 }
